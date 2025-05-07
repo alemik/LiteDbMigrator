aff3a0d5efcc0095ecc7a8313248dcc6ac301339
Michele Alessio <miky76@gmail.com>
2025-05-07 14:56:47 +0200

    ManualProject

---
+++ ManualTest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LiteDbMigrator;
+using ManualTest;
+using System.Diagnostics;
+
+Console.WriteLine("Testing Migrator");
+
+try
+{
+    using var db = new LiteDB.LiteDatabase(DbSettings.DbPath);
+
+    var migrator = new Migrator(db)
+        .Collection("travels")
+        .ForEachInArray("Places", sub => sub.RenameField("Arrival", "StartDate"))
+        .ForEachInArray("Places", sub => sub.RenameField("Departure", "EndDate"));
+    
+    migrator.Execute();
+}
+catch (Exception ex)
+{
+    Debug.Print(ex.Message);
+}
